8da257591eec31f31a7fb41857467cabfef85726
Paul Suh <paul@mspex.net>
2024-01-16 16:00:32 -0500

    Test loading from a subdirectory

---
+++ tests/pytester_example_files/example_test_load_subdirs_tester.py
@@ -0,0 +1,2 @@
+def test_load_subdirs_tester(paramfiledata):
+    assert paramfiledata["input_data"] == paramfiledata["expected_result"]
---
+++ tests/test_load_subdirs.py
@@ -0,0 +1,16 @@
+def test_load_subdirs(pytester):
+    # create the test code file
+    test_file_path = pytester.copy_example("example_test_load_subdirs_tester.py")
+    test_file_path.rename("test_load_subdirs_tester.py")
+
+    # create the data files
+    pytester.copy_example("data_load_subdirs_tester_1.yaml")
+
+    datafile2_path = pytester.copy_example("data_load_subdirs_tester_2.json")
+    subdir_path = datafile2_path.parent / "subdir"
+    subdir_path.mkdir()
+    datafile2_path.rename(subdir_path / datafile2_path.name)
+
+    result = pytester.runpytest("-k", "test_load_subdirs_tester", "--param-from-files", "-v")
+
+    result.assert_outcomes(passed=4)
