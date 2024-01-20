3349ce258681b9962817c3ad49e1a80eb0b6415a
Paul Suh <paul@mspex.net>
2024-01-20 01:07:51 -0500

    Refactor plugin code

--- src/pytest_parameterize_from_files/plugin.py
+++ src/pytest_parameterize_from_files/plugin.py
@@ -28,6 +28,14 @@ def pytest_addoption(parser):
 
 
 def _load_test_data_from_file(filepath: str) -> dict[str, Any]:
+    """Load test data from a file and return it as a dictionary.
+
+    :param filepath: The path of the file to load.
+    :type filepath: str
+    :return: The loaded test data as a dictionary.
+    :rtype: dict[str, Any]
+    :raises BadTestCaseData: If the loaded test data is not a dictionary or any of the case data is not a dictionary.
+    """
     with open(filepath) as fp:
         if filepath.endswith(".json"):
             test_data = load(fp)
@@ -42,6 +50,28 @@ def _load_test_data_from_file(filepath: str) -> dict[str, Any]:
                 raise BadTestCaseData(f"From {filepath}: data for case {case_name} is not a dict. ")
 
         return test_data
+
+
+def _locate_and_load_test_data(test_name: str) -> dict[str, dict[str, Any]]:
+    """Locates and loads test data for the given test name.
+
+    :param test_name: The name of the test.
+    :return: A dictionary containing the loaded test data.
+    """
+    result = {}
+    for root, dirs, files in os.walk(os.getcwd()):
+        # remove dirs that start with .
+        for one_dir in dirs:
+            if one_dir.startswith("."):
+                dirs.remove(one_dir)
+
+        test_data_filenames = [one_filename for one_filename in files if one_filename.startswith("data_" + test_name)]
+
+        for one_data_file in test_data_filenames:
+            test_data = _load_test_data_from_file(join(root, one_data_file))
+            result |= test_data
+
+    return result
 
 
 def _extract_fixture_names(fixture_dict: dict[str, dict[str, Any]]) -> list[str]:
@@ -73,6 +103,32 @@ def _extract_fixture_names(fixture_dict: dict[str, dict[str, Any]]) -> list[str]
     return sorted(all_fixture_keys)
 
 
+def _extract_fixture_data(fixture_raw_data_dict: dict[str, dict[str, Any]]) -> tuple[list[str], list[list[Any]]]:
+    """Extracts fixture data into a format ready for the parameterize call.
+
+    metafunc.parameterize() expects a list of fixture names, a list of lists of data, and an optional list
+    of case id's. _extract_fixture_names() gets the names, this function sets up the other two lists.
+
+    :param fixture_raw_data_dict: A dictionary containing fixture data, where the keys are test case IDs and the
+                                  values are dictionaries mapping fixture names to fixture data.
+    :type fixture_raw_data_dict: dict[str, dict[str, Any]]
+    :return: A tuple containing the list of test case IDs and a list of lists representing the fixture data.
+    :rtype: tuple[list[str], list[list[Any]]]
+    """
+    # this sets up a dict of dicts by case id, with the keys of each sub-dict being the
+    # fixture names in alphabetical order
+    fixture_cases_dict = {
+        test_case_id: {key: test_case_data[key] for key in sorted(fixture_raw_data_dict[test_case_id])}
+        for test_case_id, test_case_data in fixture_raw_data_dict.items()
+    }
+    # derive a list of lists from the dict of dicts. It's important to do this
+    # so that the lists are in the same order as the case id's
+    fixture_data_list = [
+        [test_case_values for test_case_values in test_case.values()] for test_case in fixture_cases_dict.values()
+    ]
+    return list(fixture_cases_dict.keys()), fixture_data_list
+
+
 def pytest_generate_tests(metafunc):
     """Hook called by Pytest for each test.
 
@@ -91,18 +147,7 @@ def pytest_generate_tests(metafunc):
     # parameterize against list of names if match
     test_name = metafunc.definition.name.removeprefix("test_")
 
-    fixture_raw_data_dict = {}
-    for root, dirs, files in os.walk(os.getcwd()):
-        # remove dirs that start with .
-        for one_dir in dirs:
-            if one_dir.startswith("."):
-                dirs.remove(one_dir)
-
-        test_data_filenames = [one_filename for one_filename in files if one_filename.startswith("data_" + test_name)]
-
-        for one_data_file in test_data_filenames:
-            test_data = _load_test_data_from_file(join(root, one_data_file))
-            fixture_raw_data_dict |= test_data
+    fixture_raw_data_dict = _locate_and_load_test_data(test_name)
 
     if len(fixture_raw_data_dict) > 0:
         # do processing only if the search found cases
@@ -111,17 +156,8 @@ def pytest_generate_tests(metafunc):
         # will raise an exception if the fixture names are inconsistent
         fixture_names = _extract_fixture_names(fixture_raw_data_dict)
 
-        # this sets up a dict of dicts by case id, with the keys of each sub-dict being the
-        # fixture names in alphabetical order
-        fixture_cases_dict = {
-            test_case_id: {key: test_case_data[key] for key in sorted(fixture_raw_data_dict[test_case_id])}
-            for test_case_id, test_case_data in fixture_raw_data_dict.items()
-        }
+        # reformat the case ids and fixture data into list and list of lists respectively
+        case_ids, fixture_data_list = _extract_fixture_data(fixture_raw_data_dict)
 
-        # derive a list of lists from the dict of dicts. It's important to do this
-        # so that the lists are in the same order as the case id's
-        fixture_data_list = [
-            [test_case_values for test_case_values in test_case.values()] for test_case in fixture_cases_dict.values()
-        ]
-
-        metafunc.parametrize(fixture_names, fixture_data_list, ids=fixture_cases_dict.keys(), scope="function")
+        # do the parameterization
+        metafunc.parametrize(fixture_names, fixture_data_list, ids=case_ids, scope="function")
