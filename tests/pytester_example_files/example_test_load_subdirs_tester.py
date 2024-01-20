ab16c16d993bb178bbdb9228b54217a6c705baf5
Paul Suh <paul@mspex.net>
2024-01-20 17:36:06 -0500

    Update load subdirs test

--- tests/pytester_example_files/example_test_load_subdirs_tester.py
+++ tests/pytester_example_files/example_test_load_subdirs_tester.py
@@ -1,2 +1,2 @@
-def test_load_subdirs_tester(paramfiledata):
-    assert paramfiledata["input_data"] == paramfiledata["expected_result"]
+def test_load_subdirs_tester(input_1, input_2, expected_result):
+    assert expected_result == input_1 + input_2
