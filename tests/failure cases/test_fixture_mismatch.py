001d2c2666f203ada7d18e223687bb4af0a11f18
Paul Suh <paul@mspex.net>
2024-01-27 17:26:24 -0500

    Test fixture mismatch

---
+++ tests/failure cases/test_fixture_mismatch.py
@@ -0,0 +1,11 @@
+def test_fixture_mismatch(pytester):
+    test_file_path = pytester.copy_example("example_test_fixture_mismatch_tester.py")
+    test_file_path.rename("test_fixture_mismatch_tester.py")
+
+    # create the data files
+    pytester.copy_example("data_fixture_mismatch_tester_1.yaml")
+    pytester.copy_example("data_fixture_mismatch_tester_2.yaml")
+
+    result = pytester.runpytest("-k", "test_fixture_mismatch_tester", "--param-from-files", "-v")
+
+    result.assert_outcomes(errors=1)
