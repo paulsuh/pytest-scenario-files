1684581d646b935afc1a44f718e680cd83c626e7
Paul Suh <paul@mspex.net>
2024-01-27 16:58:41 -0500

    Add test case for test case data not a dict

---
+++ tests/failure cases/test_test_case_not_dict.py
@@ -0,0 +1,10 @@
+def test_test_case_not_dict(pytester):
+    test_file_path = pytester.copy_example("example_test_test_case_not_dict_tester.py")
+    test_file_path.rename("test_test_case_not_dict_tester.py")
+
+    # create the data files
+    pytester.copy_example("data_test_case_not_dict_tester.yaml")
+
+    result = pytester.runpytest("-k", "test_test_case_not_dict_tester", "--param-from-files", "-v")
+
+    result.assert_outcomes(errors=1)
