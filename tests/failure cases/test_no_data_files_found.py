80586308a0e90506f85cbc866f33db66a543c153
Paul Suh <paul@mspex.net>
2024-01-27 17:31:25 -0500

    Add test with for data files found

---
+++ tests/failure cases/test_no_data_files_found.py
@@ -0,0 +1,9 @@
+def test_no_data_files_found(pytester):
+    test_file_path = pytester.copy_example("example_test_no_data_files_found_tester.py")
+    test_file_path.rename("test_no_data_files_found_tester.py")
+
+    # no data files!
+
+    result = pytester.runpytest("-k", "test_no_data_files_found_tester", "--param-from-files", "-v")
+
+    result.assert_outcomes(errors=1)
---
+++ tests/pytester_example_files/example_test_no_data_files_found_tester.py
@@ -0,0 +1,2 @@
+def test_no_data_files_found_tester(input_data_1, input_data_2, expected_result):
+    assert expected_result == input_data_1 + input_data_2
