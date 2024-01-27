a5a7599e26249bd00572413d0f96f69bb5a9c328
Paul Suh <paul@mspex.net>
2024-01-27 12:03:51 -0500

    Add test for merge conflict

---
+++ tests/failure cases/test_fixture_merge_conflict.py
@@ -0,0 +1,11 @@
+def test_fixture_merge_conflict(pytester):
+    test_file_path = pytester.copy_example("example_test_fixture_merge_conflict_tester.py")
+    test_file_path.rename("test_fixture_merge_conflict_tester.py")
+
+    # create the data files
+    pytester.copy_example("data_fixture_merge_conflict_tester_1.yaml")
+    pytester.copy_example("data_fixture_merge_conflict_tester_2.yaml")
+
+    result = pytester.runpytest("-k", "test_fixture_merge_conflict_tester", "--param-from-files", "-v")
+
+    result.assert_outcomes(errors=1)
---
+++ tests/pytester_example_files/example_test_fixture_merge_conflict_tester.py
@@ -0,0 +1,2 @@
+def test_fixture_merge_conflict_tester(input_data_1, input_data_2, expected_result):
+    assert expected_result == input_data_1 + input_data_2
