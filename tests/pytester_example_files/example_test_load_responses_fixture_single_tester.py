1d18449fdec3a95be785f97d2a3105c67c3b8e0b
Paul Suh <plsuh@goodeast.com>
2024-10-02 01:25:39 -0400

    Add multiple URL tests

--- tests/pytester_example_files/example_test_load_responses_fixture_single_tester.py
+++ tests/pytester_example_files/example_test_load_responses_fixture_single_tester.py
@@ -1,6 +1,6 @@
 import requests
 
 
-def test_load_responses_fixture_tester(psf_responses, expected_result):
+def test_load_responses_fixture_single_tester(expected_result):
     req_result = requests.get("https://www.example.com/")
     assert req_result.text == expected_result
--- tests/success cases/test_load_responses_fixture.py
+++ tests/success cases/test_load_responses_fixture.py
@@ -1,14 +1,27 @@
 """Test indirect fixture loading"""
 
 
-def test_load_responses_fixture(pytester):
+def test_load_responses_fixture_single(pytester):
     # create the test code file
-    test_file_path = pytester.copy_example("example_test_load_responses_fixture_tester.py")
-    test_file_path.rename("test_load_responses_fixture_tester.py")
+    test_file_path = pytester.copy_example("example_test_load_responses_fixture_single_tester.py")
+    test_file_path.rename("test_load_responses_fixture_single_tester.py")
 
     # create the data file
-    pytester.copy_example("data_load_responses_fixture_tester_1.yaml")
+    pytester.copy_example("data_load_responses_fixture_single_tester_1.yaml")
 
-    result = pytester.runpytest("-k", "test_load_responses_fixture_tester", "-v", "--psf-load-responses")
+    result = pytester.runpytest("-k", "test_load_responses_fixture_single_tester", "-v", "--psf-load-responses")
 
     result.assert_outcomes(passed=2)
+
+
+def test_load_responses_fixture_multiple(pytester):
+    # create the test code file
+    test_file_path = pytester.copy_example("example_test_load_responses_fixture_multiple_tester.py")
+    test_file_path.rename("test_load_responses_fixture_multiple_tester.py")
+
+    # create the data file
+    pytester.copy_example("data_load_responses_fixture_multiple_tester_1.yaml")
+
+    result = pytester.runpytest("-v", "--psf-load-responses")
+
+    result.assert_outcomes(passed=1)
