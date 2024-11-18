72c2eb73b4d9b5fa1d05ff7853e7f4807ae8606f
Paul Suh <plsuh@goodeast.com>
2024-11-18 00:14:23 -0500

    All other example tests

--- tests/Responses_example/test_api_connection.py
+++ tests/Responses_example/test_api_connection.py
@@ -23,19 +23,19 @@ def test_login_to_api(url_response_override, psf_expected_result, netbrain_conne
         assert netbrain_connection_obj.nb_req_headers["token"] == expected_result
 
 
-def test_get_tenant_id(psf_responses, psf_expected_result, netbrain_connection_obj):
+def test_get_tenant_id(url_response_override, psf_expected_result, netbrain_connection_obj):
     with psf_expected_result as expected_result:
         netbrain_connection_obj.get_tenant_id()
         assert netbrain_connection_obj.tenant_id == expected_result
 
 
-def test_get_domain_id(psf_responses, psf_expected_result, netbrain_connection_obj):
+def test_get_domain_id(url_response_override, psf_expected_result, netbrain_connection_obj):
     with psf_expected_result as expected_result:
         netbrain_connection_obj.get_domain_id()
         assert netbrain_connection_obj.domain_id == expected_result
 
 
-def test_set_tenant_and_domain(psf_responses, psf_expected_result, netbrain_connection_obj):
+def test_set_tenant_and_domain(url_response_override, psf_expected_result, netbrain_connection_obj):
     with psf_expected_result as expected_result:  # noqa F841
         netbrain_connection_obj.set_tenant_and_domain()
         # set_tenant_and_domain() doesn't change anything on the client side
