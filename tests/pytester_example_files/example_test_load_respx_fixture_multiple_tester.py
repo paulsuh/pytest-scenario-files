7f4d03fed2e24fcf2dca4baa9b54319616a4082b
Paul Suh <plsuh@goodeast.com>
2025-03-24 14:47:26 -0100

    Update tests to use psf_expected_result

--- tests/pytester_example_files/example_test_load_respx_fixture_multiple_tester.py
+++ tests/pytester_example_files/example_test_load_respx_fixture_multiple_tester.py
@@ -3,8 +3,8 @@ import pytest
 
 
 @pytest.mark.skipif('not config.getoption("psf-load-respx", False)')
-def test_load_respx_fixture_multiple_tester(psf_respx_mock, expected_result):
+def test_load_respx_fixture_multiple_tester(psf_respx_mock, url_tests):
     with httpx.Client() as client:
-        for one_test in expected_result:
+        for one_test in url_tests:
             req_result = client.get(one_test["url"])
             assert req_result.text == one_test["text"]
--- tests/success cases/test_load_respx_fixture.py
+++ tests/success cases/test_load_respx_fixture.py
@@ -42,3 +42,16 @@ def test_load_respx_fixture_partial(pytester):
     result = pytester.runpytest("-v", "--psf-load-respx", "--psf-fire-all-responses")
 
     result.assert_outcomes(passed=1, errors=1)
+
+
+def test_load_respx_fixture_update_response(pytester):
+    # create the test code file
+    test_file_path = pytester.copy_example("example_test_load_respx_fixture_update_response_tester.py")
+    test_file_path.rename("test_load_respx_fixture_update_response_tester.py")
+
+    # create the data file
+    pytester.copy_example("data_load_respx_fixture_update_response_tester_1.yaml")
+
+    result = pytester.runpytest("-v", "-rA", "--psf-load-respx")
+
+    result.assert_outcomes(passed=2)
