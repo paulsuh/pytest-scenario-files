e3f9c9497655cea0f76c092b3c3f8a8a54e6abc2
Paul Suh <plsuh@goodeast.com>
2025-05-05 11:05:13 -0400

    Fix missing conditional import

--- tests/failure cases/test_responses_fixture_data_not_dict_or_list.py
+++ tests/failure cases/test_responses_fixture_data_not_dict_or_list.py
@@ -1,3 +1,9 @@
+import pytest
+
+# skip these tests if the responses module is not present
+responses = pytest.importorskip("responses")
+
+
 def test_responses_fixture_data_not_dict_or_list(pytester):
     test_file_path = pytester.copy_example("example_test_responses_fixture_data_not_dict_or_list_tester.py")
     test_file_path.rename("test_responses_fixture_data_not_dict_or_list_tester.py")
