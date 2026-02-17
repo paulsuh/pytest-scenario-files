ed9c35e90be20869349a2084ab2fd01f32483f1e
Paul Suh <plsuh@goodeast.com>
2026-02-16 21:35:49 -0500

    Skip basic tests if responses or respx

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,7 @@
+from importlib.util import find_spec
+
 pytest_plugins = "pytester"
+
+# if responses or respx are loaded, ignore basic cases
+if (find_spec("responses") is not None) or (find_spec("respx") is not None):
+    collect_ignore_glob = ["basic cases/*"]
